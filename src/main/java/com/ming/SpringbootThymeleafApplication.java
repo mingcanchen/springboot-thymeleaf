937cfba9dbee27fa186b677c0fc6db5c2617879d
wolter <mingcanchen@hotmail.com>
2018-07-29 16:15:19 +0800

    thymeleaf demo

---
+++ src/main/java/com/ming/SpringbootThymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.ming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wolter
+ */
+@SpringBootApplication
+public class SpringbootThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ming/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.ming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author chenmingcan
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(ModelMap modelMap) {
+        modelMap.addAttribute("name", "ming1");
+        return "hello";
+    }
+}
